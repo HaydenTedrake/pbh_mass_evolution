c4300aaba43fbc92226f41425c245658a2621fc3
Hayden Tedrake <hayden17@mit.edu>
2025-01-14 20:55:39 -0500

    adding a new file for plotting the correct mass function since i didn't get it to work in my code

--- pbh_movie.py
+++ pbh_movie.py
@@ -1,27 +1,3 @@
-# import numpy as np
-# import matplotlib.pyplot as plt
-
-# # Parameters for the lognormal distribution
-# mu = 10**15  # Mean mass in grams
-# sigma = 2  # Standard deviation in log space
-
-# # Generate masses over a logarithmic range
-# log_mass = np.linspace(12, 19, 1000)  # log10(mass) range from 10^12 g to 10^19 g
-# mass = 10**log_mass  # Convert to linear scale
-
-# # Lognormal probability density function (PDF)
-# pdf = (1 / (mass * sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((np.log(mass / mu) / sigma) ** 2))
-
-# # Plotting the initial mass distribution
-# plt.figure(figsize=(10, 6))
-# plt.plot(log_mass, pdf, color='salmon')
-# plt.fill_between(log_mass, pdf, color='salmon', alpha=0.5)
-# plt.title("Initial Mass Distribution")
-# plt.xlabel("log10(Mass) [g]")
-# plt.ylabel("Probability Density")
-# plt.grid(True)
-# plt.show()
-
 import numpy as np
 import matplotlib.pyplot as plt
 
