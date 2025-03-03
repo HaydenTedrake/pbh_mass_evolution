2aaa6952f256cb600384860effb64faab77aebdf
Hayden Tedrake <hayden17@mit.edu>
2025-03-02 21:31:26 -0500

    new f(M)

--- f_m.py
+++ f_m.py
@@ -13,119 +13,6 @@ def gev_to_grams(energy_gev):
     
     return mass_grams
 
-def old_old_f(M):
-    """
-    Calculate f(M) with M in grams
-    """
-    # in grams from Table I
-    beta_masses = {
-        'mu': 4.53e14,     # muon
-        'u': 1.6e14,       # up quark
-        'd': 1.6e14,       # down quark
-        's': 9.6e13,       # strange quark
-        'c': 2.56e13,      # charm quark
-        'T': 2.68e13,      # tau
-        'b': 9.07e12,      # bottom quark
-        't': 0.24e12,      # top quark (unobserved at time of paper)
-        'g': 1.1e14,       # gluon (effective mass)
-        'w': 7.97e11,      # W boson
-        'z': 7.01e11,      # Z boson
-        'h': 2.25e11       # Higgs boson
-    }
-
-    print("here are the beta masses from old code")
-    print(beta_masses)
-    
-    # Base constant from the original equation
-    base = 1.569
-    
-    # Detailed calculation following the exact equation
-    result = (
-        base
-        + 0.569 * (
-            np.exp(-M / beta_masses['mu'])
-            + 3 * np.exp(-M / beta_masses['u'])
-            + 3 * np.exp(-M / beta_masses['d'])
-            + 3 * np.exp(-M / beta_masses['s'])
-            + 3 * np.exp(-M / beta_masses['c'])
-            + np.exp(-M / beta_masses['T'])
-            + 3 * np.exp(-M / beta_masses['b'])
-            + 3 * np.exp(-M / beta_masses['t'])
-            + 0.963 * np.exp(-M / beta_masses['g'])
-        )  
-        + 0.36 * np.exp(-M / beta_masses['w'])
-        + 0.18 * np.exp(-M / beta_masses['z'])
-        + 0.267 * np.exp(-M / beta_masses['h'])
-    )
-    return result
-
-def old_f(M):
-    """
-    Calculate f(M) with M in grams
-    """
-    # particle masses in GeV
-    masses = {
-        'mu': gev_to_grams(0.10566),          # muon
-        'u': gev_to_grams(0.34),              # up quark
-        'd': gev_to_grams(0.34),              # down quark
-        's': gev_to_grams(0.96),              # strange quark
-        'c': gev_to_grams(1.28),              # charm quark
-        'T': gev_to_grams(1.7768),            # tau
-        'b': gev_to_grams(4.18),              # bottom quark
-        't': gev_to_grams(173.1),             # top quark
-        'g': gev_to_grams(0.650),             # gluon
-        'w': gev_to_grams(80.433),            # W boson
-        'z': gev_to_grams(91.19),             # Z boson
-        'h': gev_to_grams(124.07),            # Higgs boson
-    }
-
-    beta_values = {
-        '0': 2.66,
-        '1/2': 4.53,
-        '1': 6.04,
-        '2': 9.56
-    }
-
-    base = 1.569
-
-    def beta_masses(mass, spin):
-        """Calculate hbar * c^3 / (8 * pi * G * mass) and return in grams."""
-        beta_mass = (hbar * c**3 * beta_values[spin]) / (8 * math.pi * G * mass)    
-        return beta_mass
-
-    print("here are the beta_mass values for new code")
-    print(beta_masses(masses['mu'], '1/2'))
-    print(beta_masses(masses['u'], '1/2'))
-    print(beta_masses(masses['d'], '1/2'))
-    print(beta_masses(masses['s'], '1/2'))
-    print(beta_masses(masses['c'], '1/2'))
-    print(beta_masses(masses['T'], '1/2'))
-    print(beta_masses(masses['b'], '1/2'))
-    print(beta_masses(masses['t'], '1/2'))
-    print(beta_masses(masses['g'], '1'))
-    print(beta_masses(masses['w'], '1'))
-    print(beta_masses(masses['z'], '1'))
-    print(beta_masses(masses['h'], '0'))
-
-    result = (
-        base
-        + 0.569 * (
-            np.exp(-M / beta_masses(masses['mu'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['u'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['d'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['s'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['c'], '1/2'))
-            + np.exp(-M / beta_masses(masses['T'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['b'], '1/2'))
-            + 3 * np.exp(-M / beta_masses(masses['t'], '1/2'))
-            + 0.963 * np.exp(-M / beta_masses(masses['g'], '1'))
-        )  
-        + 0.36 * np.exp(-M / beta_masses(masses['w'], '1'))
-        + 0.18 * np.exp(-M / beta_masses(masses['z'], '1'))
-        + 0.267 * np.exp(-M / beta_masses(masses['h'], '0'))
-    )
-    return result
-
 def f(M):
     """
     Calculate f(M) with M in grams
@@ -200,14 +87,10 @@ def f(M):
 # Define a range for M in logarithmic space
 M_values = np.logspace(9, 20, 10000)  # M values from 10^9 to 10^20
 f_values = f(M_values)
-old_old_f_values = old_old_f(M_values)
-
-print(f_values)
-print(old_old_f_values)
 
 # Plot the function using logarithmic scales
 plt.figure(figsize=(12, 8))
-plt.semilogx(M_values, new_f_values, label='$f(M)$', color='blue', linewidth=2)
+plt.semilogx(M_values, f_values, label='$f(M)$', color='blue', linewidth=2)
 
 # Customize the plot
 plt.xlabel('$M$ (Grams)', fontsize=14)
@@ -220,4 +103,4 @@ plt.legend(fontsize=12)
 plt.yticks(np.arange(2, 16, 2))  # Set y-axis ticks at intervals of 2, up to 14
 
 # Display the plot
-# plt.show()
+plt.show()
