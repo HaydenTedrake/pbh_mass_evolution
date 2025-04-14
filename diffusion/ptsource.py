8f135338285b97666169486aec44215b76a368de
Hayden Tedrake <hayden17@mit.edu>
2025-04-14 10:56:03 -0400

    adding more complicated source functions

--- diffusion/ptsource.py
+++ diffusion/ptsource.py
@@ -26,10 +26,23 @@ def laplacian(u):
     ) / dx**2
 
 # RHS function for solve_ivp
-def dudt(t, u_flat):
+def dudt(t, u_flat, kind="log", alpha=2):
     u = u_flat.reshape((nx, ny, nz))
     du = D * laplacian(u)
+    # Inject source at center
+    du[cx, cy, cz] += source_function(t, kind=kind, alpha=alpha)
     return du.ravel()
+
+# Time-dependent source function
+def source_function(t, kind="log", alpha=2):
+    if kind == "log":
+        return np.log(t + 1) * 1e-4
+    elif kind == "power":
+        return (t ** alpha) * 1e-6
+    elif kind == "impulse":
+        return 1e-3 if np.isclose(t, 5.0, atol=0.1) else 0.0
+    else:
+        return 0.0
 
 # Initial condition: delta function at the center
 u0 = np.zeros((nx, ny, nz))
@@ -37,7 +50,8 @@ u0[cx, cy, cz] = 1.0 / (dx * dy * dz)
 u0_flat = u0.ravel()
 
 # Solve the system
-sol = solve_ivp(dudt, (0, total_time), u0_flat, t_eval=t_eval, method='RK45')
+rhs = lambda t, u_flat: dudt(t, u_flat, kind="power", alpha=2)
+sol = solve_ivp(rhs, (0, total_time), u0_flat, t_eval=t_eval, method='RK23')
 
 # Precompute the slices to animate
 slices = [np.maximum(sol.y[:, i].reshape((nx, ny, nz))[:, :, nz//2], 1e-6) for i in range(steps)]
