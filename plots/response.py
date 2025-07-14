d307abc70b24ac576f90872a9c550e33c72e617f
Hayden Tedrake <hayden17@mit.edu>
2025-07-14 11:26:26 -0400

    made response

--- plots/response.py
+++ plots/response.py
@@ -6,10 +6,6 @@ import pandas as pd
 df = pd.read_csv('flux2122.csv')
 a = df['0.0000000000'].values
 
-# Load flux data
-df = pd.read_csv('flux2122.csv')
-a = df['0.0000000000'].values
-
 n = len(a)  # 20000
 
 # Load M_ext.csv
@@ -34,4 +30,16 @@ plt.ylabel('Amplitude')
 plt.legend()
 plt.grid(True)
 plt.title("Response g = M_ext @ a")
+plt.show()
+
+indices_to_plot = [0, 200, 400, 600, 800] 
+colors = ['blue', 'orange', 'green', 'red', 'purple']
+plt.figure(figsize=(10, 6))
+for idx, color in zip(indices_to_plot, colors):
+    plt.plot(M_ext[:, idx][:1000], color=color, label=f'Column {idx}')
+plt.xlabel("Row index (i)")
+plt.ylabel("Amplitude")
+plt.title("M_ext")
+plt.legend()
+plt.grid(True)
 plt.show()
